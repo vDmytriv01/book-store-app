d79b1da5c35af330e5b7754356bf98aa2e06e9f4
vDmytriv01 <vadymdmytriv@gmail.com>
2025-06-13 15:10:08 +0200

    chore: initial setup with CI, checkstyle and properties

---
+++ src/main/java/com/vdmytriv/bookstoreapp/BookstoreAppApplication.java
@@ -0,0 +1,12 @@
+package com.vdmytriv.bookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreAppApplication.class, args);
+    }
+
+}
