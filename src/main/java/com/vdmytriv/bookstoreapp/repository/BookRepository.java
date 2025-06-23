5c92dd007fa8cbe17d5213ad4cbd26a51e1b521c
Vadym Dmytriv <vadymdmytriv@gmail.com>
2025-06-23 13:42:55 +0200

    feat(book): implement entity, service, repository and configure MySQL
    * feat(book): add entity, repository and service
    
    * fix(project): apply mentor feedback
    feat(book): implement entity, service, repository and configure MySQL
    * feat(config): apply mentor feedback – switch to MySQL

---
+++ src/main/java/com/vdmytriv/bookstoreapp/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.vdmytriv.bookstoreapp.repository;
+
+import com.vdmytriv.bookstoreapp.model.Book;
+import java.util.List;
+
+public interface BookRepository {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
---
+++ src/main/java/com/vdmytriv/bookstoreapp/service/BookService.java
@@ -0,0 +1,10 @@
+package com.vdmytriv.bookstoreapp.service;
+
+import com.vdmytriv.bookstoreapp.model.Book;
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
