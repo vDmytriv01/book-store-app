291d770f70f2bb4ca24c7b73d844e6ef99543e2d
vDmytriv01 <vadymdmytriv@gmail.com>
2025-06-24 10:37:43 +0200

    feat(repository): update BookServiceImpl and BookService

--- src/main/java/com/vdmytriv/bookstoreapp/service/BookServiceImpl.java
+++ src/main/java/com/vdmytriv/bookstoreapp/service/BookServiceImpl.java
@@ -1,5 +1,9 @@
 package com.vdmytriv.bookstoreapp.service;
 
+import com.vdmytriv.bookstoreapp.dto.BookDto;
+import com.vdmytriv.bookstoreapp.dto.CreateBookRequestDto;
+import com.vdmytriv.bookstoreapp.exception.EntityNotFoundException;
+import com.vdmytriv.bookstoreapp.mapper.BookMapper;
 import com.vdmytriv.bookstoreapp.model.Book;
 import com.vdmytriv.bookstoreapp.repository.BookRepository;
 import java.util.List;
@@ -11,14 +15,25 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Book not found with id " + id));
+        return bookMapper.toDto(book);
     }
 }
