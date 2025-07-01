5b6b2956763908bd01272c0ced46e586000b5679
vDmytriv01 <vadymdmytriv@gmail.com>
2025-07-01 21:12:48 +0200

    feat(service): implement updateBook() and softDeleteBook() methods

--- src/main/java/com/vdmytriv/bookstoreapp/service/BookServiceImpl.java
+++ src/main/java/com/vdmytriv/bookstoreapp/service/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.vdmytriv.bookstoreapp.service;
 
 import com.vdmytriv.bookstoreapp.dto.BookDto;
 import com.vdmytriv.bookstoreapp.dto.CreateBookRequestDto;
+import com.vdmytriv.bookstoreapp.dto.UpdateBookRequestDto;
 import com.vdmytriv.bookstoreapp.exception.EntityNotFoundException;
 import com.vdmytriv.bookstoreapp.mapper.BookMapper;
 import com.vdmytriv.bookstoreapp.model.Book;
@@ -24,6 +25,14 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    public BookDto update(Long id, UpdateBookRequestDto requestDto) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with id " + id));
+        bookMapper.updateModel(requestDto, book);
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
     public List<BookDto> findAll() {
         return bookRepository.findAll().stream()
                 .map(bookMapper::toDto)
@@ -35,5 +44,14 @@ public class BookServiceImpl implements BookService {
         Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Book not found with id " + id));
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Book not found with id " + id)
+        );
+        book.setDeleted(true);
+        bookRepository.save(book);
     }
 }
