219c80e7c3ff08f3388748e51b55b9934603c579
vDmytriv01 <vadymdmytriv@gmail.com>
2025-07-01 21:14:31 +0200

    feat(controller): implement update and delete endpoints in BookController

--- src/main/java/com/vdmytriv/bookstoreapp/controller/BookController.java
+++ src/main/java/com/vdmytriv/bookstoreapp/controller/BookController.java
@@ -2,13 +2,16 @@ package com.vdmytriv.bookstoreapp.controller;
 
 import com.vdmytriv.bookstoreapp.dto.BookDto;
 import com.vdmytriv.bookstoreapp.dto.CreateBookRequestDto;
+import com.vdmytriv.bookstoreapp.dto.UpdateBookRequestDto;
 import com.vdmytriv.bookstoreapp.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -35,5 +38,19 @@ public class BookController {
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public BookDto updateBook(
+            @PathVariable Long id,
+            @RequestBody UpdateBookRequestDto requestDto) {
+        return bookService.update(id, requestDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBookById(@PathVariable Long id) {
+        bookService.delete(id);
     }
 }
